808c1ec97b7feb49bff1d5f61769f35c5fc64e8d
ErikMaris <77492856+ErikMaris@users.noreply.github.com>
2023-08-13 14:01:47 +0200

    updated paths for Mac use

--- config/run_at_startup.m
+++ config/run_at_startup.m
@@ -2,6 +2,6 @@
 
 % add all paths (folders named private are automatically excluded
 folder = fileparts(which(mfilename)); 
-addpath(genpath(fullfile(folder,'..\')));
+addpath(genpath(fullfile(folder,['..' filesep])));
 
-javaaddpath('..\bin\external\bfmatlab\bioformats_package.jar')
+javaaddpath('/../bin/external/bfmatlab/bioformats_package.jar')
